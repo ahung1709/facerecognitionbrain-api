3d9a6d370e9fe7a073226cef5c116373d5dd6341
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2023-11-27 15:56:39 -0800

    add logic for returning response of registered users

--- server.js
+++ server.js
@@ -84,13 +84,17 @@ app.post('/register', (req, res) => {
     //     entries: 0,
     //     joined: new Date()
     // })
-    db('users').insert({
-        email: email, 
-        name: name, 
-        joined: new Date()
-    }).then(console.log)
-
-    res.json(database.users[database.users.length-1])
+    db('users')
+        .returning('*')
+        .insert({
+            email: email, 
+            name: name, 
+            joined: new Date()
+        })
+        .then(user => {
+            res.json(user[0])
+        })
+        .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
