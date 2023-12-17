048af101dd511ff0dc23d826fef5fb9bf30f0606
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2023-12-16 20:54:44 -0800

    move api fetch function to back end from front end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,52 @@
+const returnClarifaiRequestOptions = (imageUrl) => {
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '2556c2fbcd9041cd9a5a9a2250f426f1';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'ahappswk';       
+    const APP_ID = 'test';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const IMAGE_URL = imageUrl;
+   
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+  
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    return requestOptions;
+  
+}
+
+const handleApiCall = (req, res) => {
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequestOptions(req.body.input))
+    .then(response => response.json())
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +59,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage, 
+    handleApiCall
 }
