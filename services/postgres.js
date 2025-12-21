0468fef79a8fa82270eea03fa1730b42862ca0b1
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2025-12-20 23:51:21 -0800

    update logic for setting up database to use neon database

--- services/postgres.js
+++ services/postgres.js
@@ -7,18 +7,25 @@ const connectPostgres = () => {
   if (process.env.POSTGRES_URI) {
     connection = process.env.POSTGRES_URI;
 
-    // 2. Heroku Postgres OR local development using Heroku DB
+    // 2. Production Neon Postgres OR local development using Neon DB
+  } else if (process.env.NEON_DATABASE_URL) {
+    connection = {
+      connectionString: process.env.NEON_DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    };
+
+    // 3. Production Heroku Postgres OR local development using fallback Heroku DB
   } else if (process.env.DATABASE_URL) {
     connection = {
       connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
     };
 
-    // 3. No DB configured
+    // 4. No DB configured
   } else {
     throw new Error(
       'ERROR: No database connection string found.' +
-        'Expected POSTGRES_URI (Docker) or DATABASE_URL (Heroku).'
+        'Expected POSTGRES_URI (Docker), NEON_DATABASE_URL (Neon), or DATABASE_URL (Heroku).'
     );
   }
 
